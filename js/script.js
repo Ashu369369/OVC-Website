ba131b401e1f5cfea48ea6bc44935ba590310636
Ashu369369 <aarshsingh95@gmail.com>
2024-05-02 02:10:36 -0600

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function showSidebar(){
+    sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar(){
+    sidebar = document.querySelector(".sidebar");
+    sidebar.style.display = 'none';
+}
